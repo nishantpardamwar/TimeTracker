d9fdb26933d2b112303d9e87d1eea9270f66d479
nishantpardamwar <nishantpardamwar@gmail.com>
2016-02-13 16:19:49 +0530

    added login and project list view (functionality remaining).

--- TimeTracker/TimeTracker/LoginViewController.swift
+++ TimeTracker/TimeTracker/LoginViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func loadHomeScreen(sender : UIButton){
+        
+        
+        performSegueWithIdentifier("onLoginSuccess", sender: nil)
+        
     }
-
-
+        
 }
 
